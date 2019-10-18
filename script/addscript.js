810aeba9f3d2c0d280aa58dd1c0a0c3afe9a8609
lexandrv <lexandrv@gmail.com>
2019-10-18 19:32:02 +0300

    Выполнение решения дополнительного задания к уроку 2

--- script/addscript.js
+++ script/addscript.js
@@ -1,2 +1,6 @@
 let num = 266219;
-console.log(2 * 6 * 9 * 2 * 1 * 9);
+multiNum = (2 * 6 * 9 * 2 * 1 * 9);
+console.log(multiNum);
+newNumber = multiNum ** 3;
+console.log(newNumber);
+console.log(String(newNumber).substr(0, 2));
