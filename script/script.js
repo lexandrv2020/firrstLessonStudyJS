f8a8123e86239d5a1c2e7019850fc1716c8d9ab0
lexandrv <lexandrv@gmail.com>
2019-10-18 19:18:51 +0300

    Выполнил задания урока #2

--- script/script.js
+++ script/script.js
@@ -1,91 +1,17 @@
-let income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+let money = 149000,
+    income = '140000',
+    addExpenses = 'Аренда квартиры, банковский кредит, одежда, питание, обучение, расходы на семью',
+    deposit = false,
+    mission = 2500000,
+    period = 72;
 
-/*alert('Добрый день, Куратор.');
-console.log('Жду ваших дальнейших указаний, Куратор.');*/
-/*
-console.log(true || true); //true
-console.log(true || false); //true
-console.log(false || false); //false
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log('Период месяцев: ' + period);
+console.log('Цель заработать: ' + mission + ' руб.');
+console.log(addExpenses.toLowerCase().split(', '));
 
-console.log(true && true); //true
-console.log(true && false); //false
-console.log(false && false); //false
-
-console.log(!true); //true
-console.log(!false); //false
-
-console.log(50);
-console.log(8.5);
-console.log(.5);
-console.log(Infinity);
-console.log(NaN);
-
-console.log(typeof 50);
-console.log(typeof 8.5);
-console.log(typeof .5);
-console.log(typeof Infinity);
-console.log(typeof NaN);
-
-console.log(0 / 0);
-
-console.log(+5);
-console.log(-5);
-
-let i = 5;
-console.log(i);
-console.log(--i);
-console.log(i);
-
-
-console.log(9 / 2);
-console.log(9 % 2);
-
-
-let n = 15;
-//n = n + 3;
-n += 3;
-console.log(n);
-//n = n - 6;
-n -= 6;
-console.log(n);
-//n = n / 3;
-n /= 3;
-console.log(n);
-
-
-console.log(3 > 2);
-console.log(3 < 2);
-console.log(5 >= 3);
-console.log(10 <= 5);
-console.log(5 === 5);
-console.log(5 !== 6);
-
-console.log(5 == '5');
-console.log(Math.sqrt(4));
-console.log(Math.pow(5, 3));
-*/
-let myString = 'Hello world!';
-let myStr = 'apple, kiwi, orange';
-/*console.log('myStr: ', myStr);
-
-console.log(myStr.length);
-console.log(myStr.toUpperCase());
-console.log(myStr.toLowerCase());
-///console.log(myStr.charAt(1));
-console.log(myStr[0]);
-console.log(myStr.substring(9)); //с какого символа до конца
-console.log(myStr.substring(9, 14)); //с какого символа до какого символа
-console.log(myStr.slice(6)); //с начала сколько символов
-console.log(myStr.slice(-2)); // с конца сколько символов
-console.log(myStr.substr(2, 4)); //начальный индекс, сколько символов
-console.log(myStr);
-console.log(myStr.indexOf('world')); //возвращает начальный индекс первой буквы
-console.log(myStr.indexOf('o')); //возвращает начальный индекс первой буквы
-console.log(myStr.replace('my world', 'my Friends')); //возвращает начальный индекс первой буквы*/
-
-
-console.log(myStr.split(', ')); //создает массив значений строки по разделителю
+let budgetDay = (money + Number(income)) / 30;
+console.log(budgetDay.toFixed(2));
