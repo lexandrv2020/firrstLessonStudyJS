d040f24da20dbc2afe7bd7245ea9b6d5cba4b992
lexandrv <lexandrv@gmail.com>
2019-10-19 10:35:57 +0300

    Дополнение к основному заданию и исправление решения дополнительного задания к уроку 2

--- script/script.js
+++ script/script.js
@@ -13,5 +13,10 @@ console.log('Период месяцев: ' + period);
 console.log('Цель заработать: ' + mission + ' руб.');
 console.log(addExpenses.toLowerCase().split(', '));
 
-let budgetDay = (money + Number(income)) / 30;
+let budgetMonth = (money + Number(income)); //бюджет на месяц
+let budgetDay = budgetMonth / 30; //бюджет на день
+let restOfDivision = (budgetMonth % 30); //остаток от деления
+
+//выводим в консоль дневной заработок и остаток от деления 'доход в месяц' / 'количество дней' 
 console.log(budgetDay.toFixed(2));
+console.log(restOfDivision);
