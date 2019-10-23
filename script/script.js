fab9273125edf2a3ddc308330f6c41532e7b2e1b
lexandrv <lexandrv@gmail.com>
2019-10-23 15:05:56 +0300

    Выполнение Упр.№1 дополнительного домашнего задания уроа №05

--- script/script.js
+++ script/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//1 задание
+let arr = ['2411', '121215', '4898', '65461', '24154', '85245', '2841'];
+for (let i = 0; i < arr.length; i++) {
+    let item = arr[i];
+
+    let firstSymbol = Number(item.substr(0, 1));
+    if (firstSymbol === 2 || firstSymbol === 4) {
+        console.log('item: ', item);
+    }
+}
+
+//2 задание
