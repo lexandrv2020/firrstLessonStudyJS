8c3a5268e4691b22b24b414321205349153b227c
lexandrv <lexandrv@gmail.com>
2019-10-23 08:51:44 +0300

    Исправление дополнительного домашнего задания к уроку №04

--- script/script.js
+++ script/script.js
@@ -2,14 +2,34 @@
 
 let anyValue = prompt('Введите произвольное содержимое', '   Вчера понедельник,  сегодня вторник, а завтра среда.... Пятница не за горами... ');
 
+
 const newString = function(longString) {
     return longString.slice(0, 30) + '...'; //сокращаем строку
 }
 
-let postAnyValue = function(anyValue) {
-    if (typeof(anyValue) !== 'string') {
-        console.log('Тип веденного значения не равен строке. Условие обязательное.');
+function getTypeOfValue(anyValue) {
+
+    if (typeof anyValue === 'object') {
+        //пользователь ввел отмена
+        alert('Обязательно условие: ввести строку и подтвердить, а не нажать отмена.');
+        return false;
+
     } else {
+        let newType = Math.abs(anyValue);
+        if (String(newType) === 'NaN') {
+            return true;
+        }
+    }
+    alert('Обязательно условие: ввести строку и подтвердить.');
+    return false;
+}
+
+
+let postAnyValue = function(anyValue) {
+
+    let typeOfValueIsString = getTypeOfValue(anyValue);
+
+    if (typeOfValueIsString) {
         let formattedString = anyValue.replace(/^\s+/g, '');
         if (formattedString.length > 30) {
             formattedString = newString(formattedString);
