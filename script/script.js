0bb96e625f9f8ca384bdee681f8c2b6e8a30b29a
lexandrv <lexandrv@gmail.com>
2019-10-24 10:21:03 +0300

    Выполнение бонусного задания №1. Задача №3

--- script/script.js
+++ script/script.js
@@ -1,52 +1,30 @@
 'use strict';
-/*
-let firstYear,
-    secondYear,
-    questin_1 = 'Введите начальный год',
-    questin_2 = 'Введите конечный год',
-    arrYears;
+
+let itemNumber,
+    sumNumber = 0,
+    question = 'Введите число';
 
 do {
-    firstYear = prompt(questin_1, '')
-    if (isNotNumber(firstYear)) {
-        questin_1 = 'Вы ввели не числовое значение. Год вводится числом. Повторите ввод.';
+    itemNumber = prompt(question, '')
+    if (isNotNumber(itemNumber)) {
+        question = 'Вы ввели не число число';
+    } else if (!(isCanseled(itemNumber))) {
+        question = 'Введите следующее число';
+    } else {
+        alert('Сумма введенных Вами чисел равна ', sumNumber);
+        break;
     }
+
+    sumNumber += Number(itemNumber);
 }
-while (isNotNumber(firstYear));
+while (!isCanseled(itemNumber));
 
-do {
-    secondYear = prompt(questin_2, '')
-    if (isNotNumber(secondYear)) {
-        questin_2 = 'Вы ввели не числовое значение конечного года, повторите ввод.';
-    } else if (firstYear === secondYear) {
-        alert(`Оба года равны. (${firstYear} = ${secondYear}). Введите значение отличного от начального года`);
-    }
-}
-while (isNotNumber(secondYear) || (firstYear === secondYear));
-
-let result = firstYear - secondYear;
-
-arrYears = getHighYears((result < 0) ? firstYear : secondYear, (result < 0) ? secondYear : firstYear);
-console.log(arrYears);
+alert(`Сумма введенных Вами чисел равна ${sumNumber}.`);
 
 function isNotNumber(value) {
     return (isNaN(value) || value === '' || value === null);
 }
 
-function getHighYears(valuemin, valuemax) {
-    let arrYears = new Array();
-    for (let f = Number(valuemin); f <= valuemax; f++) {
-
-        if (f % 4 > 0) {
-            //не делится на 4 - не высокосный
-        } else if ((f % 100 === 0) && (f % 400 === 0)) {
-            // (+) делится на 100 и  делится на 400 - высокосный 
-            arrYears[arrYears.length] = f;
-        } else if (f % 100 > 0) {
-            // (+) не делится на 100 - высокосный 
-            arrYears[arrYears.length] = f;
-        }
-
-    }
-    return arrYears;
-}*/
+function isCanseled(value) {
+    return (value === null);
+}
