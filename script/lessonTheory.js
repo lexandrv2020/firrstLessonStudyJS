795e8655cc2573d48f4320433471110ca3320697
lexandrv <lexandrv@gmail.com>
2019-10-25 13:14:08 +0300

    прослушивание лекции

--- script/lessonTheory.js
+++ script/lessonTheory.js
@@ -8,8 +8,32 @@
 //let car = {}; (1)
 let car = { //(2)
     model: 'mazda',
-    year: 2006
+    year: 2006,
+    style: {
+        color: 'green'
+    }
 };
 let obj = new Object();
+obj.color = 'black';
+//console.log('obj: ', obj);
+car.style = obj; //ссылка на объект
+//console.log('car: ', car);
+//console.log('car.style = obj: ', car.style === obj);
+//console.log('obj: ', obj);
+car.style.color = 'green'; //поменяли цвет у *obj* по ссылке
+//console.log('obj: ', obj);
+obj.color = 'red';
 
+car.ride = function(speed) {
+    console.log('Машина едет со скоростью ' + speed + ' км/ч');
+};
+car.ride(60);
+
+car.stop = stop;
+car.stop();
+stop();
+
+function stop() {
+    console.log('Машина стоит со скоростью 0 км/ч');
+}
 console.log('car: ', car);
