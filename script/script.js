d4d5580075d0dcf4375111b46e54691627fee37a
lexandrv <lexandrv@gmail.com>
2019-10-25 19:15:40 +0300

    Выполнение дополнительного домашнего задания 06

--- script/script.js
+++ script/script.js
@@ -1,100 +1,27 @@
 'use strict';
 
-let money,
-    start = function() {
-        do {
-            money = prompt('Ваш месячный доход?', 289000);
-        }
-        while (isNaN(money) || money === '' || money === null);
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+let div = document.getElementById('weekDay');
+let today = new Date();
+let day = today.getDay();
+
+for (let i = 0; i < week.length; i++) {
+    console.log(week[i], i);
+
+    if (i + 1 === day) {
+        div.innerHTML += "<b>" + week[i] + "</b><br>";
+    } else if (i + 1 === 6 || i + 1 === 7) {
+        div.innerHTML += "<em>" + week[i] + "</em><br>";
+    } else {
+        div.innerHTML += week[i] + "<br>";
     }
 
-start();
 
-let appData = {
-
-    income: {},
-    addIncome: [],
-    expenses: {},
-    addExpenses: [],
-    deposit: false,
-    mission: 2500000,
-    period: 72,
-    budget: Number(money),
-    budgetDay: 0,
-    budgetMonth: 0,
-    expensesMonth: 0,
-    accumulatedMonth: 0,
-    asking: function() {
-        let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Аренда квартиры, банковский кредит, одежда, питание, обучение, расходы на семью');
-        appData.addExpenses = addExpenses.toLowerCase().split(',');
-        appData.deposit = confirm('Есть ли у вас депозит в банке?');
-
-        let listExpenses,
-            valueExpenses,
-            objExpenses = {};
-
-        for (let i = 0; i < 2; i++) {
-            if (i === 0) {
-                listExpenses = prompt('Какие обязательные ежемесячные расходы у вас есть?', 'Ипотека');
-            } else {
-                listExpenses = prompt('Какие еще обязательные ежемесячные расходы у вас есть?', 'Дети: обучение, развлечения');
-            }
-            do { valueExpenses = prompt('А во сколько это обойдется?', 90000) }
-            while (isNaN(valueExpenses) || valueExpenses === '' || valueExpenses === null);
-            objExpenses[listExpenses] = Number(valueExpenses);
-        }
-        appData.expenses = objExpenses;
-        //console.log('objExpenses: ', objExpenses);
-
-    },
-    getExpensesMonth: function() {
-        let sumExpenses = 0;
-        for (let key in appData.expenses) {
-            sumExpenses += appData.expenses[key];
-        }
-        appData.expensesMonth = sumExpenses
-    },
-    getBudget: function() {
-        appData.accumulatedMonth = appData.budget - appData.expensesMonth;
-        appData.budgetMonth = appData.budget - appData.expensesMonth;
-        appData.budgetDay = Math.floor(appData.budgetMonth / 30);
-    },
-    getTargetMonth: function() {
-        return Math.floor(appData.mission / appData.accumulatedMonth);
-    },
-    getStatusIncome: function() {
-        if (appData.budgetDay >= 800) {
-            return 'Высокий уровень дохода';
-        } else if ((appData.budgetDay < 800) && (appData.budgetDay > 300)) {
-            return 'Средний уровень дохода';
-        } else if ((appData.budgetDay <= 300) && (appData.budgetDay > 0)) {
-            return 'Низкий уровень дохода';
-        } else if (appData.budgetDay === 0) {
-            return 'Жаль, не остается денег на отпуск';
-        } else if (appData.budgetDay < 0) {
-            return 'Что-то в этой жизни пошло не так.....';
-        }
-    }
 }
 
-appData.asking();
-appData.getExpensesMonth();
-appData.getBudget();
-let targetMonth = appData.getTargetMonth();
-let statusIncome = appData.getStatusIncome();
 
-//console.log(`Доход за месяц: ${money} рублей.`);
-console.log(`Расходы за месяц: ${appData.expensesMonth} рублей`);
-//console.log(`Накопления за месяц: ${appData.accumulatedMonth} рублей`);
-console.log(`Срок достижения цели накопления в ${appData.mission} руб.: ${targetMonth} месяцев`);
-//console.log(`Цель ${(targetMonth > 0) ? "когда-то :) " : "НИКОГДА :( не"} будет достигнута....`);
-if (appData.budgetDay < 0) {
-    console.log(statusIncome + ' (дневной заработок' + appData.budgetDay + ' руб.)');
-} else {
-    console.log('Оценка текущего дневного заработка: ', statusIncome + ' (' + appData.budgetDay + ' руб.)');
-}
 
-console.log("Наша программа включает в себя данные:");
-for (let element in appData) {
-    console.log("   " + element + ": " + appData[element]);
-}
+//console.log('today: ', today);
+
+//console.log('day: ', day);
