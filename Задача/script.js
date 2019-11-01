876522f137fb906f2fe8dfcaa83b5b719f367723
lexandrv <lexandrv@gmail.com>
2019-11-01 07:52:24 +0300

    Подготовка к уроку №10

--- Задача/script.js
+++ Задача/script.js
@@ -1,19 +1,17 @@
-btn = document.getElementsByTagName('.btn')[0];
-console.log('btn: ', btn);
-
+btn = document.getElementsByTagName('button')[0];
 btn.addEventListener('click', start);
 
 function start() {
     let arr = [];
     let newArr = [];
-    let txt = document.querySelector('.text');
-    //txt.textContent = arr;
+    let txt_1 = document.querySelector('.firstArr');
+    let txt_2 = document.querySelector('.secondArr');
     for (let i = 1; i < 10; i++) {
         arr.push(Math.ceil(Math.random() * (16 - 1) + 1));
     }
     console.log('arr: ', arr);
-    // alert(arr);
-    //div.textConte
+    txt_1.textContent = "Массив 1: " + arr;
+
     for (let i = 0; i < arr.length; i++) {
         let sliceArr = arr.slice(i + 1, arr.length);
         let num = 0;
@@ -24,6 +22,7 @@ function start() {
         });
         newArr.push(num);
     }
-    console.log('newArr: ', newArr);
+    console.log('Массив 2: ', newArr);
+    txt_2.textContent = 'Массив 2: ' + newArr;
 
 };
