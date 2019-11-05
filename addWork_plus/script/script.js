b745d88e932b8c43857aad64258eb21ca9d40ddd
lexandrv <lexandrv@gmail.com>
2019-11-05 19:39:38 +0300

    Домашняя работа  №10

--- addWork_plus/script/script.js
+++ addWork_plus/script/script.js
@@ -1,6 +1,18 @@
 'use strict';
 
+document.addEventListener("DOMContentLoaded", function() {
+    const domElement = new DomElement('.divSquare', '100px', '100px', 'green', '24');
+    domElement.addElement();
+});
+
 let elementBody = document.querySelector('body');
+let _div = document.createElement('div');
+
+let _right = 0;
+let _left = 0;
+let _up = 0;
+let _down = 0;
+
 
 class DomElement {
     constructor(selector, height, width, bg, fontSize) {
@@ -12,18 +24,31 @@ class DomElement {
     }
     addElement() {
         if (this.selector !== '') {
-            let _div = document.createElement('div');
+
             elementBody.append(_div);
             _div.style.height = this.height;
             _div.style.width = this.width;
             _div.style.background = this.bg;
-            _div.style.fontSize = this.height;
-            _div.style.position = 'absolut';
+            _div.style.position = 'absolute';
         }
         console.log(this);
     }
 }
 
+function onkeydown(event) {
+    //    console.log(event.code);
+    if (event.code === 'ArrowRight') {
+        _div.style.left = _div.offsetLeft + 10 + 'px';
+    } else if (event.code === 'ArrowLeft') {
+        _div.style.left = _div.offsetLeft - 10 + 'px';
+    } else if (event.code === 'ArrowUp') {
+        _div.style.top = _div.offsetTop - 10 + 'px';
+    } else if (event.code === 'ArrowDown') {
+        _div.style.top = _div.offsetTop + 10 + 'px';
+    }
+}
 
-const domElement2 = new DomElement('.divSquare', '100px', '100px', 'green', '100');
-domElement2.addElement();
+
+
+
+document.addEventListener('keydown', onkeydown);
