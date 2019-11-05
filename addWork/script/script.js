aa55eeff52f9d3438c3b6c9a28a5336da71222f6
lexandrv <lexandrv@gmail.com>
2019-11-05 18:57:19 +0300

    Домашняя работа  №10

--- addWork/script/script.js
+++ addWork/script/script.js
@@ -1,15 +1,15 @@
 'use strict';
 
-let elementSection = document.querySelector('section');
+let elementSection = document.querySelector('body');
 console.log('elementSection: ', elementSection);
 
 class DomElement {
     constructor(selector, height, width, bg, fontSize) {
         this.selector = selector;
-        this.height = +height;
-        this.width = +width;
+        this.height = height;
+        this.width = width;
         this.bg = bg;
-        this.fontSize = +fontSize;
+        this.fontSize = fontSize;
     }
     addElement() {
         if (this.selector !== '') {
@@ -20,7 +20,7 @@ class DomElement {
                 _div.style.height = this.height;
                 _div.style.width = this.width;
                 _div.style.background = this.bg;
-                _div.style.fontSize = this.height;
+                _div.style.fontSize = this.fontSize;
             } else if (this.selector.substring(0, 1) === '#') {
                 let _p = document.createElement('p');
                 _p.innerHTML = 'Так как первый символ <strong># (решетка)</strong>, -  мы добавили <strong>"p" (параграф)</strong>.';
@@ -28,12 +28,12 @@ class DomElement {
                 _p.style.height = this.height;
                 _p.style.width = this.width;
                 _p.style.background = this.bg;
-                _p.style.fontSize = this.height;
+                _p.style.fontSize = this.fontSize;
             }
             console.log(this);
         }
     }
 }
 
-const domElement2 = new DomElement('.divElement', '200', '200', 'green', '100');
+const domElement2 = new DomElement('#divElement', '150px', '800px', 'green', '20px');
 domElement2.addElement();
