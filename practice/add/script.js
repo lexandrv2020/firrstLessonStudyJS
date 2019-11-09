3c5dd13e95e936c41617f51ef371b3e6063022fb
lexandrv <lexandrv@gmail.com>
2019-11-09 14:12:44 +0300

    Выполнение усложненного ДЗ №12

--- practice/add/script.js
+++ practice/add/script.js
@@ -1,43 +1,42 @@
-//setTimeout, setInterval и requestAnimationFrame
 'use strict';
 
-let currentTime = document.getElementsByTagName('h1'),
-    divElements = document.querySelectorAll('div'),
-    textHello = document.querySelector('.hello'),
-    getDay = document.querySelector('.getDay'),
-    getTime = document.querySelector('.getTime'),
-    toNewYear = document.querySelector('.toNewYear'),
-    arrDays = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-    curentDate = new Date(),
-    curentDay = curentDate.getDay(),
-    curentHour = curentDate.getHours(),
-    curentMinute = curentDate.getMinutes(),
-    curentSecond = curentDate.getSeconds(),
-    am_pm = curentHour >= 12 ? 'PM' : 'AM',
-    newYear = new Date('01 jan 2020 00:00:00'),
-    timeRemaining = (newYear - curentDate) / 1000,
-    daysRemaining = Math.floor(timeRemaining / 60 / 60 / 24);
+let btnStart = document.querySelector('.start'),
+    btnCancel = document.querySelector('.cancel'),
+    mosq = document.querySelector('#mosq'),
+    count = 0,
+    flying = false;
 
-currentTime[0].textContent = curentDate;
-//console.log('curentDay: ', curentDay);
-if (curentHour >= 22 & curentHour < 6) {
-    textHello.textContent = 'Доброй ночи';
-} else if (curentHour >= 6 & curentHour < 10) {
-    textHello.textContent = 'Доброе утро';
-} else if (curentHour >= 12 & curentHour < 18) {
-    textHello.textContent = 'Добрый день';
-} else if (curentHour >= 18 & curentHour < 22) {
-    textHello.textContent = 'Добрый вечер';
-};
+let moveInterval;
 
-getDay.textContent = 'Сегодня: ' + arrDays[curentDay + 1];
-curentHour = (curentHour < 10) ? '0' + curentHour : curentHour;
-curentMinute = (curentMinute < 10) ? '0' + curentMinute : curentMinute;
-curentSecond = (curentSecond < 10) ? '0' + curentSecond : curentSecond;
-getTime.textContent = 'Текущее время: ' + curentHour + ':' + curentMinute + ':' + curentSecond + ' ' + am_pm;
-toNewYear.textContent = 'До нового года осталось: ' + daysRemaining + ' дн.';
+let animeMosq = function() {
 
-console.log('divElements: ', divElements);
-divElements.forEach(function(item) {
-    item.style.fontStyle = 'italic';
-});
+    moveInterval = requestAnimationFrame(animeMosq);
+
+    count++;
+    if (count < 500) {
+        mosq.style.right = count + 'px';
+    } else {
+        cancelAnimationFrame(moveInterval);
+    }
+}
+
+let resetAll = function() {
+    count = 0;
+    mosq.style.right = count + 'px';
+    flying = false;
+    cancelAnimationFrame(moveInterval);
+}
+
+function startMove() {
+    if (flying === false) {
+        moveInterval = requestAnimationFrame(animeMosq);
+        flying = true;
+    } else {
+        flying = false;
+        cancelAnimationFrame(moveInterval);
+    }
+
+}
+
+btnStart.addEventListener('click', startMove);
+btnCancel.addEventListener('click', resetAll);
