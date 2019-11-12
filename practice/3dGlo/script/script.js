5f2f47ae932de81f837ad77d43f308d00cb7ff4d
lexandrv <lexandrv@gmail.com>
2019-11-12 18:32:56 +0300

    Домашнее заданиеe усложненное  №14

--- practice/3dGlo/script/script.js
+++ practice/3dGlo/script/script.js
@@ -45,13 +45,21 @@ window.addEventListener('DOMContentLoaded', function() {
 
         const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
+            body = document.querySelector('body'),
             menuItems = menu.querySelectorAll('ul>li'),
             document_a = document.querySelectorAll('a');
 
-        const actionMenu = () => {
+        const actionMenu = (event) => {
             menu.classList.toggle('active-menu');
         };
 
+        const onMouseClick = (event) => {
+            let active_menu = document.querySelector('.active-menu');
+            let closeMenu = !(event.target.classList.contains('active-menu')) && (!!active_menu) && (event.target.localName !== 'img');
+            if (closeMenu) {
+                menu.classList.toggle('active-menu');
+            }
+        }
         const openBlock = (event, btn) => {
             event.preventDefault()
             const blockId = btn.getAttribute('href');
@@ -61,22 +69,34 @@ window.addEventListener('DOMContentLoaded', function() {
             });
         };
 
+        const applyEvent = (event, btn) => {
+            if (event === 'actionMenu') {
+                return actionMenu;
+            } else if (event === 'mouseclick') {
+                return onMouseClick;
+            } else {
+                return openBlock(event, btn);
+            }
+        };
+
         btnMenu.addEventListener('click', actionMenu);
         menuItems.forEach(element => {
-            element.addEventListener('click', actionMenu);
+            element.addEventListener('click', applyEvent('actionMenu', element));
         });
-
+        body.addEventListener('click', applyEvent('mouseclick', body));
         document_a.forEach(element => {
             if (element.classList.contains('close-btn')) {
-                element.addEventListener('click', actionMenu);
+                element.addEventListener('click', applyEvent('actionMenu', element));
             } else if (element.closest('li') || element.closest('main')) {
-
                 element.addEventListener('click', function(event) {
-                    openBlock(event, element)
+                    applyEvent(event, element)
                 });
             }
         });
+
+
     };
+
     toggleMenu();
 
     //popup
@@ -121,7 +141,6 @@ window.addEventListener('DOMContentLoaded', function() {
                     popup.style.display = 'none';
                 }
             }
-
         });
     }
     togglePopUp();
@@ -156,10 +175,7 @@ window.addEventListener('DOMContentLoaded', function() {
                     }
                 });
             }
-
         });
-
-
     }
     tabs();
 
