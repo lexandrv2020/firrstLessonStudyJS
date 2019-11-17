755df1ceb1c82da77635936f02c7b30382f78d54
lexandrv <lexandrv@gmail.com>
2019-11-17 16:33:20 +0300

    Оптимизация TODO

--- to do/script/script.js
+++ to do/script/script.js
@@ -2,37 +2,21 @@
 
 const listItem = document.querySelector('.listItems'),
     text = document.querySelector('.newItem'),
-    btn = document.querySelector('button');
+    btn = document.querySelector('button'),
+    items = document.querySelectorAll('.item');
 
-let setAttributesForLi = () => {
-    let item = document.querySelectorAll('.item');
-    console.log(item);
-    item.forEach(element => {
-        element.addEventListener('click', () => {
-            if (element.hasAttribute('style')) {
-                element.removeAttribute('style')
-            } else {
-                element.setAttribute('style', 'text-decoration:line-through; font-weight: 700');
-            }
-        });
-    });
+function setStatus() {
+    console.log('target.classList: ', this);
+    this.classList.toggle('crosstext');
 };
 
 let createItem = () => {
     let newLi = document.createElement('li');
     newLi.classList.add('item');
     newLi.textContent = text.value.trim();
-    newLi.addEventListener('click', () => {
-        if (newLi.hasAttribute('style')) {
-            newLi.removeAttribute('style')
-        } else {
-            newLi.setAttribute('style', 'text-decoration:line-through; font-weight: 700');
-        }
-    })
+    newLi.addEventListener('click', setStatus);
     listItem.append(newLi);
-
-
-}
+};
 
 btn.addEventListener('click', () => {
     if (text.value === '') {
@@ -43,4 +27,14 @@ btn.addEventListener('click', () => {
     text.value = '';
 });
 
-setAttributesForLi();
+const initial = () => {
+    const style = document.createElement('style');
+    style.id = 'cross-text';
+    style.textContent = '.crosstext {text-decoration:line-through; font-weight: 700}';
+    document.head.appendChild(style);
+
+    items.forEach(element => {
+        element.addEventListener('click', setStatus);
+    });
+};
+initial();
