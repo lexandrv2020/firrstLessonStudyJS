1db1fe37205748d725f708e3f91b6452b978aeb6
lexandrv <lexandrv@gmail.com>
2019-11-17 13:35:51 +0300

    Выполнение задания TO DO

--- to do/script/script.js
+++ to do/script/script.js
@@ -1,33 +1,46 @@
 'use strict';
 
-function getResult(a, b) {
-    let result360 = 360;
-    let н_1 = 30;
-    let м_1 = 6;
-    let м_h_1 = 0.5;
-    let angH = ((a <= 12) ? a : a - 12) * (+н_1);
-    let angM = м_1 * +b;
-    let angM_H = (+м_h_1) * (+b);
-    let _h = angH + angM_H;
-    let result1 = (_h > angM) ? (angM - _h) : (angM - _h);
-    let result2 = (+result1 > 180) ? (360 - (+result1)) : result1;
-    res.value = (result2 < 0) ? (-result2) : result2;
+const listItem = document.querySelector('.listItems'),
+    text = document.querySelector('.newItem'),
+    btn = document.querySelector('button');
+
+let setAttributesForLi = () => {
+    let item = document.querySelectorAll('.item');
+    console.log(item);
+    item.forEach(element => {
+        element.addEventListener('click', () => {
+            if (element.hasAttribute('style')) {
+                element.removeAttribute('style')
+            } else {
+                element.setAttribute('style', 'text-decoration:line-through; font-weight: 700');
+            }
+        });
+    });
+};
+
+let createItem = () => {
+    let newLi = document.createElement('li');
+    newLi.classList.add('item');
+    newLi.textContent = text.value.trim();
+    newLi.addEventListener('click', () => {
+        if (newLi.hasAttribute('style')) {
+            newLi.removeAttribute('style')
+        } else {
+            newLi.setAttribute('style', 'text-decoration:line-through; font-weight: 700');
+        }
+    })
+    listItem.append(newLi);
+
 
 }
 
-let elem_num1 = document.getElementById('a'),
-    elem_num2 = document.getElementById('b'),
-    btn_main = document.getElementById('sumMulti'),
-    res = document.getElementById('res');
-
-elem_num1.addEventListener('input', function() {
-    elem_num1.value = elem_num1.value.replace(/[^0-9]/, '');
-});
-elem_num2.addEventListener('input', function() {
-    elem_num2.value = elem_num2.value.replace(/[^0-9]/, '');
+btn.addEventListener('click', () => {
+    if (text.value === '') {
+        alert('Не заполнено содержание задачи. Повторите ввод');
+        return;
+    }
+    createItem();
+    text.value = '';
 });
 
-function newGetResult() {
-    getResult(elem_num1.value, elem_num2.value);
-}
-btn_main.addEventListener('click', newGetResult);
+setAttributesForLi();
