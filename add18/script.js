4e94c91e410484d1b101f88ba0fa7909fcbce632
lexandrv <lexandrv@gmail.com>
2019-11-22 18:46:55 +0300

    Выполнение дополнительного ДЗ №18

---
+++ add18/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    select.addEventListener('change', () => {
+
+        getAuto(select.selectedIndex)
+            .then((data) => {
+                data.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const { brand, model, price } = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+                    }
+                });
+            })
+            .catch(output.innerHTML = 'Произошла ошибка')
+            //   .finally(alert('...посмотрим другую?')
+    });
+    const getAuto = (indexItem) => {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './cars.json');
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send();
+            request.addEventListener('readystatechange', () => {
+                if (request.readyState !== 4) {
+                    return;
+                }
+                if (request.status === 200) {
+                    const data = JSON.parse(request.responseText);
+                    resolve(data);
+                } else {
+                    reject(request.statusText);
+                }
+            });
+        })
+    }
+});
